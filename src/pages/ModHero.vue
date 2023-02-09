76fdd754b0290b5b15afea6f6ad9c7055304cb50
Gabriel <gaabrielserraa@gmail.com>
2023-02-09 02:35:04 -0400

    Agregado ruta para modificar (2/2) Modulo editar listo

--- src/pages/ModHero.vue
+++ src/pages/ModHero.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { ref } from "@vue/reactivity";
 import { db } from "../firebase";
-import { collection, getDocs, query, deleteDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  query,
+  deleteDoc,
+  doc,
+  where,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import { useQuasar } from "quasar";
 
 getHero();
@@ -34,7 +43,88 @@ const columns = [
 
 const rows = ref([]);
 const loading = ref(true);
+const prompt = ref(false);
 const $q = useQuasar();
+
+const idHero = ref("");
+const nombreHero = ref("");
+const descHero = ref("");
+
+function editHero(IdHero) {
+  prompt.value = true;
+
+  try {
+    const Coleccion = collection(db, "heroes");
+    const q = query(Coleccion, where("__name__", "==", IdHero.trim()));
+
+    onSnapshot(q, (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const rowHero = {
+          IdHero: doc.id,
+          NombreHero: doc.data().nombre,
+          DescHero: doc.data().descripcion,
+        };
+
+        idHero.value = rowHero.IdHero;
+        nombreHero.value = rowHero.NombreHero;
+        descHero.value = rowHero.DescHero;
+      });
+    });
+  } catch (error) {
+    $q.notify({
+      message: error.message,
+      color: "primary",
+      icon: "error",
+    });
+    text.value = "";
+  }
+}
+
+function modHero() {
+  // console.log(idHero.value);
+  // console.log(nombreHero.value);
+  // console.log(descHero.value);
+
+  if (nombreHero.value.trim() !== "" && descHero.value.trim() !== "") {
+
+    async function updateHero() {
+      try {
+        const RefHero = doc(db, "heroes", idHero.value);
+
+        await updateDoc(RefHero, {
+          nombre: nombreHero.value,
+          descripcion: descHero.value
+        });
+
+        rows.value = [];
+        idHero.value = '';
+        nombreHero.value = '';
+        descHero.value = '';
+        getHero();
+
+        $q.notify({
+          message: "Superhéroe modificado correctamente",
+          color: "positive",
+          icon: "cloud_done",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    updateHero();
+  } else {
+    $q.notify({
+      message: "No se permiten datos vacíos",
+      color: "primary",
+      icon: "error",
+    });
+
+    prompt.value = false;
+    idHero.value = "";
+    nombreHero.value = "";
+    descHero.value = "";
+  }
+}
 
 function confirmDelete(IdHero) {
   $q.dialog({
@@ -130,7 +220,7 @@ async function getHero() {
               color="primary"
               round
               dense
-              @click=""
+              @click="editHero(props.row.IdHero)"
               icon="edit_note"
             />
             <q-btn
@@ -148,5 +238,46 @@ async function getHero() {
         </q-tr>
       </template>
     </q-table>
+
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6 text-primary">
+            Datos a editar <q-icon name="mdi-human-edit" />
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-form @submit.prevent="modHero" @key.enter="modHero">
+            <q-input
+              autofocus
+              dense
+              v-model="nombreHero"
+              label="Nombre"
+              :rules="[(val) => !!val || 'Ingresa un nombre']"
+            />
+            <q-input
+              autofocus
+              dense
+              type="textarea"
+              v-model="descHero"
+              label="Descripción"
+              :rules="[(val) => !!val || 'Ingresa una descripcion']"
+            />
+          </q-form>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancelar" v-close-popup />
+          <q-btn
+            flat
+            type="submit"
+            label="Guardar"
+            @click="modHero"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
